f4fd1aa3fc27c2bc5b7faf2b4b43225494d7cb27
muhafash <dev.muhfarash@gmail.com>
2023-06-23 02:02:28 +0700

    mengubah name image yg dikirim sesuai tanpa di encrypt"

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
+const fs = require("fs");
+const path = require("path");
 router.get("/", (req, res, next) => {
   const { page, total } = req.query;
   res.send({
@@ -22,13 +24,17 @@ router.get("/products/:id", (req, res) => {
 router.post("/products/", upload.single("image"), (req, res, next) => {
   const { name, price, stock, status } = req.body;
   const image = req.file;
-  res.json({
-    name,
-    price,
-    stock,
-    status,
-    image,
-  });
+  if (image) {
+    const target = path.join(__dirname, "uploads", image.originalname);
+    fs.renameSync(image.path, target);
+    res.json({
+      name,
+      price,
+      stock,
+      status,
+      image,
+    });
+  }
 });
 
 router.get("/:categories/:tag", (req, res) => {
